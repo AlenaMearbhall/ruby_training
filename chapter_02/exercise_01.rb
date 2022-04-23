2485374bd9a0ef1827978f330498682220a4222f
AlenaMearbhall <mearbhall.ua@gmail.com>
2022-04-23 11:56:54 +0300

    chapter 02

---
+++ chapter_02/exercise_01.rb
@@ -0,0 +1,16 @@
+class User
+end
+
+class Computer
+end
+
+class Network
+end
+
+class Host
+ end
+
+anna = User.new
+pc = Computer.new
+point = Network.new
+winner = Host.new
---
+++ chapter_02/exercise_02.rb
@@ -0,0 +1,12 @@
+class User
+  def fio
+    puts "Anna Sirko"
+  end
+  def profession
+    puts "engineer"
+  end
+end
+
+anna = User.new
+anna.fio
+anna.profession
